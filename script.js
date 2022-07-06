8943de25c5dde526537dd18e49d5096b54b0dc94
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-07-07 00:18:49 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-var system=Math.floor(Math.random()*100)+1
-var guess=0
-function guessNumber(){
+var button1 = document.getElementById('p1btn');
+var button2 = document.getElementById('p2btn');
+var score1=0;
+var score2=0;
+var randomnumber1=0
+var randomnumber2=0
+var player1=1
+var player2=1
+var winningScore = 20
 
-    var num = document.getElementById('in').value
-    if(num === '' || num >100 || num<1){
-        comsole.log('Invalid Input!! Try Again');
+var display1 = document.getElementById('randomnumberP1');
+var display2 = document.getElementById('randomnumberP2');
+
+var scoreDisplay1 = document.querySelector(".p1s")
+var scoreDisplay2 = document.querySelector(".p2s")
+
+var winner=document.getElementById('display');
+
+
+button1.addEventListener('click',function(){
+    if(player1 === 1){
+        randomnumber1 = Math.floor(Math.random()*6)+1;
+        score1 += randomnumber1
+
+        display1.innerHTML=randomnumber1;
+        scoreDisplay1.innerHTML = score1;
+
+        player1 = 0;
+        player2 = 1;
+        gameStatus()
+
     }
-    else{
-        guess++
-        var num = parseInt(document.getElementById('in').value)
-        if(num === system)
-        {
-            console.log('Congratulations!! You got the answer correctly in '+guess+ '  turns');
-            document.getElementById('out').innerHTML= '<b>Congratulations!! You got the answer correctly in '+guess+ '  turns</b> '+
-            '<br><b>Your Score is '+ guess*10 + ' out of 100</b>'
-        }
-        else if(num > system){
-            console.log('Guess a Smaller Number');
-            document.getElementById('out').innerHTML='<i>Guess a Smaller Number</i>'
-        }
-        else {
-            console.log('Guess a Greater Number');
-            document.getElementById('out').innerHTML='<i>Guess a Greater Number</i>'
-        }
+})
+button2.addEventListener('click',function(){
+    if(player2 === 1){
+        randomnumber2 = Math.floor(Math.random()*6)+1;
+        score2 += randomnumber2
+
+        display2.innerHTML=randomnumber2;
+        scoreDisplay2.innerHTML = score2;
+
+        
+        player1 = 1;
+        player2 = 0;
+        gameStatus()
+    }
+})
+
+function gameStatus(){
+    
+    if(score1 >= winningScore){
+        winner.innerHTML="Player1 win";
+        player1 = 0;
+        player2 = 0;
+    }
+    if(score2 >= winningScore){
+        winner.innerHTML="Player2 win";
+        player1 = 0;
+        player2 = 0;
     }
 }
